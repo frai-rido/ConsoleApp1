3a2f101a3840f9192218f593fa8968357dd1036d
Екатерина Чистова <rido.frai@mail.ru>
2020-10-22 12:20:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            
+            MyName = "Kate";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
