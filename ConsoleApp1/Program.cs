bf2ca579042cbede6e671c97114d7264e782e83e
Екатерина Чистова <rido.frai@mail.ru>
2020-10-22 17:52:46 +0300

    Добавила небольшую анкету

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string MyName = "Kate";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
 
-            Console.WriteLine(MyName);
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do i have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
 
             Console.ReadKey();
         }
